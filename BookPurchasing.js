614cb877d2b5cd7655774f59c2adc1510e37d49a
Faisal-style <faisalrezarahmat1@gmail.com>
2022-11-02 10:11:14 +0700

    Task js day 3

--- BookPurchasing.js
+++ BookPurchasing.js
@@ -1,37 +1,42 @@
 function BookPurchase({ name, genre, price, stock, purchased }, discount, tax) {
-  pajak = (10 / 100) * price;
-  let total = 0;
-  diskon = (10 / 100) * price;
-  priceafterdiscount = price - diskon;
-  priceaftertax = priceafterdiscount + pajak;
-  let text;
-  console.log("Nama Buku : ", name);
-  console.log("Genre Buku : ", genre);
-  console.log("Amount Of Discount : ", diskon);
-  console.log("Purchased : ", purchased);
-  console.log("Stock : ", stock);
-  console.log("Price After Discount : ", priceafterdiscount);
-  console.log("price After Tax : ", priceaftertax);
-  for (let index = 1; index <= purchased; index++) {
-    total = total + priceaftertax;
+  if (stock > 0) {
+    let total = 0;
+    diskon = (discount / 100) * price;
+    priceafterdiscount = price - diskon;
+    pajak = (tax / 100) * priceafterdiscount;
+    priceaftertax = priceafterdiscount + pajak;
+    let text;
+    for (let index = 1; index <= purchased; index++) {
+      total = total + priceaftertax;
 
-    if (index == stock) {
-      text = "can't purchased again";
-      break;
-    } else if (index <= stock) {
-      text = "You can Purchase This Product again";
+      if (index == stock) {
+        text = "can't purchased again";
+        break;
+      } else if (index <= stock) {
+        text = "You can Purchase This Product again";
+      }
     }
+    return total;
+  } else {
+    return "Stock = 0";
   }
-  console.log("Total Harga : ", total);
-  console.log("Status : ", text);
 }
 
 const bookDetail = {
   name: "Pengantar Ekonometrika",
   genre: "encyclopedia",
-  price: 200000,
-  purchased: 10,
-  stock: 10,
+  price: 10000,
+  purchased: 5,
+  stock: 5,
 };
 
-BookPurchase(bookDetail, 10, 10);
+function termOfcredit(total, term) {
+  priceperterm = total / term;
+  objectofterm = [];
+  for (let index = 0; index < term; index++) {
+    objectofterm.push({ index: index + 1, term: priceperterm });
+  }
+  return objectofterm;
+}
+
+console.log(termOfcredit(BookPurchase(bookDetail, 10, 10), 5));
