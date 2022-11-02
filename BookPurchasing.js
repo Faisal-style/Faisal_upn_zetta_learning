3a0765a99ce5ce709c771221c473c109079b9db0
Faisal-style <faisalrezarahmat1@gmail.com>
2022-11-02 10:10:02 +0700

    js day 2

--- BookPurchasing.js
+++ BookPurchasing.js
@@ -1,18 +1,37 @@
-function BookPurchase({ name, genre, price }, discount, tax) {
+function BookPurchase({ name, genre, price, stock, purchased }, discount, tax) {
   pajak = (10 / 100) * price;
+  let total = 0;
   diskon = (10 / 100) * price;
   priceafterdiscount = price - diskon;
+  priceaftertax = priceafterdiscount + pajak;
+  let text;
   console.log("Nama Buku : ", name);
   console.log("Genre Buku : ", genre);
   console.log("Amount Of Discount : ", diskon);
+  console.log("Purchased : ", purchased);
+  console.log("Stock : ", stock);
   console.log("Price After Discount : ", priceafterdiscount);
-  console.log("price After Tax : ", priceafterdiscount + pajak);
+  console.log("price After Tax : ", priceaftertax);
+  for (let index = 1; index <= purchased; index++) {
+    total = total + priceaftertax;
+
+    if (index == stock) {
+      text = "can't purchased again";
+      break;
+    } else if (index <= stock) {
+      text = "You can Purchase This Product again";
+    }
+  }
+  console.log("Total Harga : ", total);
+  console.log("Status : ", text);
 }
 
 const bookDetail = {
   name: "Pengantar Ekonometrika",
   genre: "encyclopedia",
   price: 200000,
+  purchased: 10,
+  stock: 10,
 };
 
 BookPurchase(bookDetail, 10, 10);
