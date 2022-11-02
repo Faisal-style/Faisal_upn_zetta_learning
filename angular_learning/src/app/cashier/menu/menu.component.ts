471a34c41f3eeab03a6543b7733983960491f457
Faisal-style <faisalrezarahmat1@gmail.com>
2022-11-02 21:20:59 +0700

    Task angular day 2

--- angular_learning/src/app/cashier/menu/menu.component.ts
+++ angular_learning/src/app/cashier/menu/menu.component.ts
@@ -1,12 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, getNgModuleById, OnInit } from '@angular/core';
+export interface item{id:number, name:string, harga:number}
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
+  public items : item[] = [
+    {id : 1, name :"Coffe Latte", harga: 10000},
+    {id : 2, name :"Coffe Latte", harga: 10000},
+    {id : 3, name :"Coffe Latte", harga: 10000},
+    {id : 4, name :"Coffe Latte", harga: 10000},
+    {id : 5, name :"Coffe Latte", harga: 10000},
+    {id : 6, name :"Coffe Latte", harga: 10000},
+    {id : 7, name :"Coffe Latte", harga: 10000},
+    {id : 8, name :"Coffe Latte", harga: 10000},
+    {id : 9, name :"Coffe Latte", harga: 10000},
+    {id : 10, name :"Coffe Latte", harga: 10000},
+    {id : 11, name :"Coffe Latte", harga: 10000},
+    {id : 12, name :"Coffe Latte", harga: 10000},
+  ]
   constructor() { }
 
   ngOnInit(): void {
