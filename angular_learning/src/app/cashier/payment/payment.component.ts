df4f4e35c8b0dd8b3bdb3450a1e278b97286d174
Faisal-style <faisalrezarahmat1@gmail.com>
2022-11-04 09:08:17 +0700

    Task angular day 3

--- angular_learning/src/app/cashier/payment/payment.component.ts
+++ angular_learning/src/app/cashier/payment/payment.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { outputAst } from '@angular/compiler';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { SelectedItem } from '../menu/menu.component';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.css']
+  styleUrls: ['./payment.component.css'],
 })
 export class PaymentComponent implements OnInit {
+  @Input() public payments: SelectedItem[] = [];
+  @Output() itemchanges: EventEmitter<SelectedItem[]> = new EventEmitter<
+    SelectedItem[]
+  >();
 
-  constructor() { }
+  // private selectedItemSource: selectedItem = new selectedItem([]);
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  removeItem(itemToBeRemoved: SelectedItem) {
+    const itemIndex = this.payments.findIndex(
+      ({ id }) => id === itemToBeRemoved.id
+    );
+    if (this.payments[itemIndex].amount > 1) {
+      this.payments[itemIndex].amount -= 1;
+    } else {
+      this.payments.splice(itemIndex, 1);
+      console.log(this.payments);
+    }
   }
-
 }
