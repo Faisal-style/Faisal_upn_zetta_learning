cc17af81bfccfe2065714380bb3f86ea7724c9a1
Faisal-style <faisalrezarahmat1@gmail.com>
2022-11-04 09:12:31 +0700

    angular task 3

--- angular_learning/src/app/app.component.ts
+++ angular_learning/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular_learning';
+  dataPayment: { id: number; name: string; harga: number; amount: number }[] =
+    [];
+  receiveMenu($event: any) {
+    this.dataPayment = $event;
+    console.log(this.dataPayment);
+  }
 }
--- angular_learning/src/app/cashier/cashier.module.ts
+++ angular_learning/src/app/cashier/cashier.module.ts
@@ -3,15 +3,8 @@ import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { PaymentComponent } from './payment/payment.component';
 
-
-
 @NgModule({
-  declarations: [
-    MenuComponent,
-    PaymentComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [],
+  imports: [CommonModule],
 })
-export class CashierModule { }
+export class CashierModule {}
